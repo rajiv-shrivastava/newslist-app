35cefa08762bdbce7778d7dbf1091409e6d3b22a
Rajiv Shrivastava <rjv834@gmail.com>
2022-12-09 14:59:50 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes ,Link} from 'react-router-dom'
+import NewsHomePage from "./NewsHomePage";
+import NewsDetailsPage from "./News/NewsDetailsPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const Topics = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
+
+const Home = ({ match }) => (
+  <div>
+    dfdf
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
+
+const App = () =>
+  <Router>
+      <Routes>
+      <Route exact path="/" element={<NewsHomePage/>}/>
+      <Route exact path="/newsDetail/:id" element={<NewsDetailsPage/>}/>
+      </Routes>
+  </Router>;
 
 export default App;
