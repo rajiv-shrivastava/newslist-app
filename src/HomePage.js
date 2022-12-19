bccd1256b30253578e34c78e64bd296ca61c2444
Rajiv Shrivastava <rjv834@gmail.com>
2022-12-19 18:57:11 +0530

    added colors

--- src/HomePage.js
+++ src/HomePage.js
@@ -11,7 +11,7 @@ const HomePage = (props) => {
          <hr />
           <Row> 
             <Col md={3}>               
-              <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }}>
+              <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }} >
                 <Card.Body>
                   <Card.Title>UseEffect</Card.Title>
                   <Card.Text>
@@ -26,51 +26,62 @@ const HomePage = (props) => {
               </Card>
             </Col>
             <Col md={3}>               
-             <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }}>
+             <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }} >
                 <Card.Body>
                   <Card.Title>Axios with debounce</Card.Title>
                   <Card.Text>
                     A simple http call with axios http using input debounce
                   </Card.Text>
-                  <Link to="/axiosdebounce"><Button variant="primary">View</Button></Link>
+                  <Link to="/axiosdebounce"><Button variant="danger">View</Button></Link>
                 </Card.Body>
               </Card>
             </Col>
             <Col md={3}>               
-             <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }}>
+             <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }} >
                 <Card.Body>
                   <Card.Title>React Higher Order Component</Card.Title>
                   <Card.Text>
                     A simple example of HOC where some additional custom props are added via HOC
                   </Card.Text>
                   <Link to="/higherordercomponent">
-                     <Button variant="primary">View</Button>
+                     <Button variant="warning">View</Button>
                   </Link>
                 </Card.Body>
               </Card>
             </Col>
       
             <Col md={3}>               
-             <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }}>
+             <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }} >
                 <Card.Body>
                   <Card.Title>React Router Private Route Demo</Card.Title>
                   <Card.Text>
                     A simple NoAuthPage component to demstrate private route
                   </Card.Text>
                   <Link to="/noauthpage"> 
-                  <Button variant="primary">View</Button>
+                  <Button variant="success">View</Button>
                   </Link>
                 </Card.Body>
               </Card>
             </Col>
             <Col md={3}>               
-             <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }}>
+             <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }} >
                 <Card.Body>
                   <Card.Title>React Jest</Card.Title>
                   <Card.Text>
                    A simple Login Page component where all test cases are written via Jest
                   </Card.Text>
-                  <Link to="/login"><Button variant="primary">View</Button></Link>
+                  <Link to="/login"><Button variant="secondary">View</Button></Link>
+                </Card.Body>
+              </Card>                 
+            </Col>
+            <Col md={3}>               
+             <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }} >
+                <Card.Body>
+                  <Card.Title>useMemo</Card.Title>
+                  <Card.Text>
+                   A component where data array is passed as an prop to show Memoization via useMemo
+                  </Card.Text>
+                  <Link to="/usememoeg"><Button variant="info">View</Button></Link>
                 </Card.Body>
               </Card>                 
             </Col>
