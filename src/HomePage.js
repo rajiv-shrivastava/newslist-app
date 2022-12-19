c36199ea382fa6fa9e5bf2c1ea63aea98bcb861c
Rajiv Shrivastava <rjv834@gmail.com>
2022-12-19 18:51:54 +0530

    added cardsin home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,36 +1,81 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import {Row,Col,Card,Button} from 'react-bootstrap';
 
 const HomePage = (props) => {
     
     console.log(props.children)
-    return(<>
+    return(
+      <div className="container-fluid">
          <h1 className="text-center"> Home Page component</h1>
          <hr />
-         <div className='container-fluid'>
-         <ul>
-            <li className="nav-item">
-              <Link to="/useeffecttypes"> UseEffectTypes</Link>
-            </li>            
-            <li className="nav-item">
-             <Link to="/usememoeg"> Use Memo Example</Link>
-            </li>
-            <li className="nav-item">
-             <Link to="/axiosdebounce"> Axios api call with UseEffect and Custome useDebounce</Link>
-            </li>
-            <li className="nav-item">
-             <Link to="/higherordercomponent"> React Higher Order Component</Link>
-            </li>
-            <li className="nav-item">
-             <Link to="/noauthpage"> No Auth Page</Link>
-            </li>           
-            <li className="nav-item">
-             <Link to="/login"> Login Page with test cases via Jest </Link>
-            </li>            
-            </ul>
-            </div>
-            {props.children}
-        </>
+          <Row> 
+            <Col md={3}>               
+              <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }}>
+                <Card.Body>
+                  <Card.Title>UseEffect</Card.Title>
+                  <Card.Text>
+                    Various variation of UseEffect
+                  </Card.Text>
+                  <Link to="/useeffecttypes">
+                  <Button variant="primary">    
+                    View
+                  </Button>
+                  </Link>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={3}>               
+             <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }}>
+                <Card.Body>
+                  <Card.Title>Axios with debounce</Card.Title>
+                  <Card.Text>
+                    A simple http call with axios http using input debounce
+                  </Card.Text>
+                  <Link to="/axiosdebounce"><Button variant="primary">View</Button></Link>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={3}>               
+             <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }}>
+                <Card.Body>
+                  <Card.Title>React Higher Order Component</Card.Title>
+                  <Card.Text>
+                    A simple example of HOC where some additional custom props are added via HOC
+                  </Card.Text>
+                  <Link to="/higherordercomponent">
+                     <Button variant="primary">View</Button>
+                  </Link>
+                </Card.Body>
+              </Card>
+            </Col>
+      
+            <Col md={3}>               
+             <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }}>
+                <Card.Body>
+                  <Card.Title>React Router Private Route Demo</Card.Title>
+                  <Card.Text>
+                    A simple NoAuthPage component to demstrate private route
+                  </Card.Text>
+                  <Link to="/noauthpage"> 
+                  <Button variant="primary">View</Button>
+                  </Link>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={3}>               
+             <Card style={{ height: '12rem',margin: '0px 0px 20px 0px' }}>
+                <Card.Body>
+                  <Card.Title>React Jest</Card.Title>
+                  <Card.Text>
+                   A simple Login Page component where all test cases are written via Jest
+                  </Card.Text>
+                  <Link to="/login"><Button variant="primary">View</Button></Link>
+                </Card.Body>
+              </Card>                 
+            </Col>
+          </Row>
+        </div>
     )
 }
 
